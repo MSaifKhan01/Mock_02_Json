64355a5cebda1b41354bfe01829ab9e6505268b0
MSaifKhan01 <msaifkhan5038@gmail.com>
2023-07-03 12:56:30 +0530

    Json_Server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const jsonServer=require("json-server")
+var cors = require('cors')
 
 const server=jsonServer.create();
 const router=jsonServer.router("db.json")
 const middleware=jsonServer.defaults();
 const port =process.env.PORT||3001
+
+server.use(cors())
 server.use(middleware);
 server.use(router)
 server.listen(port,()=>{
